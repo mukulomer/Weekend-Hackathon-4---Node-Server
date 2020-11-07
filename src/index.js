af7930e5c97cad682e75d567961cbc629a1ba678
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-11-07 19:06:03 +0530

    change1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 var http = require("http");
 
 const httpServer = http.createServer(handleServer);
-
+ 
+let obj= {
+    phone: '18602100000',
+    email: 'guestcaredominos@jublfood.com'
+    };
 
 function handleServer(req, res) {
-  
-}
 
+    
+    if(rq.url ==="/welcome")
+    {
+        rs.write("Welcome to Dominos!");
+        rs.end();
+    } 
+    if(rq.url ==="/contact")
+    {
+        rs.write(json.strigify(obj));
+        rs.end();
+    }
+    
+    if(rq.url !== "/welcome" && rq.url !== "/contact"  )
+    {
+    rs.status(404).send(` not found!`);
+    }
+
+
+
+}
+server.listen(8081);
 module.exports = httpServer;
